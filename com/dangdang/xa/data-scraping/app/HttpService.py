296ab84a7ac2b0454d478bdb11ee43e659ca7dbe
jiaxinyang <jiaxinyang@dangdang.com>
2021-08-16 17:21:46 +0800

    支持手机

--- com/dangdang/xa/data-scraping/app/HttpService.py
+++ com/dangdang/xa/data-scraping/app/HttpService.py
@@ -18,15 +18,34 @@ def run():
     passwd = json_obj.get("password")
     products = json_obj.get("products")
     port = json_obj.get("port")
-    task_id=json_obj.get("task_id")
-    task_label=json_obj.get("task_label")
-    threading.Thread(target=async_run, args=( account, passwd, products, port,task_id,task_label)).start()
+    task_id = json_obj.get("task_id")
+    task_label = json_obj.get("task_label")
+    threading.Thread(target=async_run, args=(account, passwd, products, port, task_id, task_label)).start()
     data = {'data': "1"}
     return json.dumps(data, ensure_ascii=False)
 
 
-def async_run( account, passwd, products, port,task_id,task_label):
-    cmt.process_data( account, passwd, products, port,task_id,task_label)
+@app.route("/process_phone", methods=["POST"])
+def run_phone():
+    get_data = request.get_data()
+    json_obj = json.loads(get_data)
+    account = json_obj.get("account")
+    products = json_obj.get("products")
+    addr = json_obj.get("addr")
+    port = json_obj.get("port")
+    task_id = json_obj.get("task_id")
+    task_label = json_obj.get("task_label")
+    threading.Thread(target=async_run_phone, args=(addr, account, products, task_id, task_label, addr, port)).start()
+    data = {'data': "1"}
+    return json.dumps(data, ensure_ascii=False)
+
+
+def async_run_phone(addr, account, products, task_id, task_label, ip, port):
+    cmt.run(addr, "0", account, products, task_id, task_label, ip, port, True)
+
+
+def async_run(account, passwd, products, port, task_id, task_label):
+    cmt.process_data(account, passwd, products, port, task_id, task_label)
 
 
 @app.route("/heart", methods=["POST"])
@@ -35,9 +54,11 @@ def heart():
     json_obj = json.loads(get_data)
     number = json_obj.get("number")
     account = json_obj.get("account")
-    port=json_obj.get("port")
-    cmt.heart(number, account,port)
+    port = json_obj.get("port")
+    cmt.heart(number, account, port)
     return "1"
+
+
 @app.route("/test", methods=["GET"])
 def test():
     return "success"
