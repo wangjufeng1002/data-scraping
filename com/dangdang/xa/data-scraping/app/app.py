2eb78118b7a974202805a4d876726b500ac4d589
wangjufeng <wangjufeng@dangdang.com>
2021-10-14 11:35:35 +0800

    feature:扫一扫修改

--- com/dangdang/xa/data-scraping/app/app.py
+++ com/dangdang/xa/data-scraping/app/app.py
@@ -149,6 +149,8 @@ def click_search(devices, name, random_policy, ip, port, account, phone):
         ##devices.xpath(
          #   '//*[@resource-id="com.taobao.taobao:id/sv_search_view"]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[5]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]').click()
         while True:
+            if  devices.xpath("扫一扫").exists is False:
+                go_back_home(devices)
             devices.xpath("扫一扫").parent().click()
             time.sleep(0.1)
             if devices.xpath('//*[@resource-id="com.taobao.taobao:id/searchbtn"]').exists is False:
--- com/dangdang/xa/data-scraping/app/db.py
+++ com/dangdang/xa/data-scraping/app/db.py
@@ -3,6 +3,7 @@ import datetime
 
 
 def update_info(origin_text, item_id, task_id, task_label,sku):
+    origin_text= origin_text.replace("'","").replace("\\","")
     with UsingMysql()as um:
         if sku is not None:
             sql = "update product_record set  original_info='{}',`status`=2 where item_id='{}'and sku_id='{}' and task_id='{}' and task_label='{}'".format(
